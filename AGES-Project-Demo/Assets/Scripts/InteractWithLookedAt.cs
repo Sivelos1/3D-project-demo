2f689a91506457841ebe38d0f4e133c315c20c10
Devin Amlen <Devin.Amlen@iam.local>
2019-03-07 17:53:43 -0600

    Implemented Interactible Objects and Interactivity Functionality

---
+++ AGES-Project-Demo/Assets/Scripts/InteractWithLookedAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects when a player presses the Interact button when looking at an IInteractable, and calls the IInteractable's InteractWith function.
+/// </summary>
+
+public class InteractWithLookedAt : MonoBehaviour
+{
+
+    [SerializeField]
+    private VisionBasedDetection detectLookedAtInteractive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && detectLookedAtInteractive.LookedAtInteractive != null)
+        {
+            Debug.Log("player did the shit");
+            detectLookedAtInteractive.LookedAtInteractive.interactWith();
+        }
+    }
+}
